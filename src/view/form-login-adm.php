cc9643df9874442161ba0cb17b4f1a50f412d63b
Programador Web 2019.2 <16124512019.2@senacrjedu.local>
2019-12-04 14:02:24 -0300

    Criação de voluntario.php e mudança de laytou login

--- src/view/form-login-adm.php
+++ src/view/form-login-adm.php
@@ -3,48 +3,21 @@
 include "view/header.php";
 
 ?>
-
-<div class='row'>
-  <div class='container'>
-    <div class='col-md-12 d-flex justify-content-center formadm'>
-      <form class="form-horizontal " method="post" action="adm">
-        <fieldset>
-
-          <!-- Form Name -->
-
-          <legend class="d-flex justify-content-center ">
-            <img src="<?php echo $url; ?>/img/doe-final.png" Class="w-75 d-flex justify-content-center ml-5" alt="">
-          </legend>
-
-          <!-- Text input-->
-          <div class="form-group formadm">
-
-            <input id="nome" name="nome" type="text" placeholder="Usuario" class="form-control input-md" required="">
-
-
-          </div>
-
-          <!-- Text input-->
-          <div class="form-group formadm">
-
-            <input id="senha" name="senha" type="password" placeholder="senha" class="form-control input-md" required="">
-
-
-          </div>
-
-          <div class="form-group ">
-            <label class=" control-label" for="cadastrar"></label>
-
-            <button id="login" style="width:388px" name="login" class="btn btn-primary formadm">Entrar</button>
-            <a class="d-flex justify-content-center " href="">
-              <p>Não tenho uma conta</p>
-            </a>
-          </div>
-
-      </form>
-      </div>
+<div class="row">
+  <div class="col-4">
+    <div class="list-group list-group-horizontal" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Cliente</a>
+      <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Administrador</a>
     </div>
   </div>
+</div>
+  <div class="col-8">
+    <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
+      <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
+    </div>
+  </div>
+</div>
 
   <?php
 
