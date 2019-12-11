7413de29851d9529865ffb46d31c9a60c2628f16
Programador Web 2019.2 <16124512019.2@senacrjedu.local>
2019-12-11 14:15:57 -0300

    testes

--- www/DAO/DAOCliente.php
+++ www/DAO/DAOCliente.php
@@ -1,49 +1,70 @@
 <?php
 
 namespace LOJA\DAO;
+
 use LOJA\Model\Conexao;
 use LOJA\Model\Cliente;
 
-class DAOCliente{
-    public function cadastrar(Cliente $cliente){
-        $sql = "INSERT INTO cliente VALUES (default, :nome, :telefone, :email,:cpf, :rua, :complemento, :cep, :uf, :bairro)";
-        $con = conexao::getInstance()->prepare($sql);
-        $con->bindValue(":nome", $cliente->getNome());
-        $con->bindValue(":telefone", $cliente->getTelefone());
-        $con->bindValue(":email", $cliente->getEmail());
-        $con->bindValue(":cpf", $cliente->getCpf());
-        $con->bindValue(":rua", $cliente->getRua());
-        $con->bindValue(":complemento", $cliente->getComplemento());
-        $con->bindValue(":cep", $cliente->getCep());
-        $con->bindValue(":uf", $cliente->getUf());
-        $con->bindValue(":bairro", $cliente->getBairro());
-        $con->execute();
-        return "Cadastrado com sucesso";
+class DAOCliente
+{
+    public function cadastrar(Cliente $cliente)
+    {
+
+        $pdo = Conexao::getInstance();
+        $pdo->beginTransaction();
+
+        try {
+            $con = $pdo->prepare(
+                "INSERT INTO cliente VALUES (default, :nome, :telefone, :cpf,:email, :rua, :complemento, :cep, :uf, :bairro)"
+            );
+
+ 
+            $con->bindValue(":nome", $cliente->getNome());
+            $con->bindValue(":telefone", $cliente->getTelefone());
+            $con->bindValue(":email", $cliente->getEmail());
+            $con->bindValue(":cpf", $cliente->getCpf());
+            $con->bindValue(":rua", $cliente->getRua());
+            $con->bindValue(":complemento", $cliente->getComplemento());
+            $con->bindValue(":cep", $cliente->getCep());
+            $con->bindValue(":uf", $cliente->getUf());
+            $con->bindValue(":bairro", $cliente->getBairro());
+            $con->execute();
+
+            $lastId = $pdo->lastInsertId(); // Retorna o id do cliente cadastrado
+            $pdo->rollback(); // Finaliza a transação
+            return "Cadastrado com sucesso";
+        } catch (\Exception $e) {
+            $pdo->rollback();
+            return "Erro ao cadastrar";
+        }
     }
-    public function listaClientes(){
+    public function listaClientes()
+    {
         $sql = "SELECT * FROM cliente";
         $con = Conexao::getInstance()->prepare($sql);
         $con->execute();
 
         $lista = array();
 
-        while($cliente = $con->fetch(\PDO::FETCH_ASSOC)){
+        while ($cliente = $con->fetch(\PDO::FETCH_ASSOC)) {
             $lista[] = $cliente;
         }
         return $lista;
     }
-    public function buscarPorId($id){
-        $sql ="SELECT * FROM cliente WHERE pk_id = :id";
+    public function buscarPorId($id)
+    {
+        $sql = "SELECT * FROM cliente WHERE pk_id = :id";
         $con = Conexao::getInstance()->prepare($sql);
         $con->bindValue(":id", $id);
         $con->execute();
 
         $cliente = new Cliente();
         $cliente = $con->fetch(\PDO::FETCH_ASSOC);
-       // print_r($usuario); //testar saida
+        // print_r($usuario); //testar saida
         return $cliente;
     }
-    public function deleteAll(){
+    public function deleteAll()
+    {
         $sql = "DELETE FROM cliente";
         $con = Conexao::getInstance()->prepare($sql);
         $con->execute();
--- www/includes/Config.php
+++ www/includes/Config.php
@@ -15,13 +15,12 @@ class Config{
     public function __construct(){
         if($this->servidor=="TEST"){
             $this->serverHost = "localhost";
-
             $this->serverDB = "loja";
             $this->serverUser = "root";
             $this->serverPass = ""; 
             $this->cepOrigem = "21852070";                       
         }else{
-            $this->$url = "http://www.doemais.web70113.uni5.net";
+            $this->url = "http://www.doemais.web70113.uni5.net";
             $this->serverHost = "mysql18-farm70.uni5.net";
             $this->serverDB = "doemais";
             $this->serverUser = "doemais";
