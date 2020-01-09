d4af0035eae4ed8a7e6f18526823eefc7e6c9be0
rodolfo ferrari <rodolfof2016@gmail.com>
2020-01-09 17:03:21 -0300

    erro no github

---
+++ www/kaio/github.php
@@ -0,0 +1,3 @@
+https://kaiocampos:Cerbero010203@github.com/kaiocampos/lojaphpunit.git
+remover o origin antigo e add o novo, quando tiver dando erro de permissao
+git remote add origin https://kaiocampos:Cerbero010203@github.com/kaiocampos/lojaphpunit.git
