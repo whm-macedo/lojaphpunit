181238767fe2dd5560b92ab8763da817bc549984
Roberto Rabelo <Roberto.junior.gonzaga@gmail.com>
2020-01-16 23:49:20 -0300

    finalizando pagamento

--- www/view/projeto.php
+++ www/view/projeto.php
@@ -29,6 +29,17 @@
       </div>
     </div>
   </div>
+  <section class="page-section" id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h2 class="section-heading text-uppercase">About</h2>
+          <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      </div>
+    
+  </section>
 
   <!-- FIM JUMBOTRON "IMPORTANCIA DE DOAR SANGUE" -->
 
