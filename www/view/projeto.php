5e9662c13caa8a40c031259b5d7bcca3c9969b6a
kaiocampos <kaiocamposti@gmail.com>
2020-01-22 15:18:39 -0300

    view projeto

--- www/view/projeto.php
+++ www/view/projeto.php
@@ -3,7 +3,7 @@
 
 <!-- INICIO JUMBOTRON "IMPORTANCIA DE DOAR SANGUE" -->
 <div class="jumbotron">
-<section class="page-section" id="about">
+  <section class="page-section" id="about">
     <div class="container">
       <div class="row">
         <div class="col-lg-12 text-center">
@@ -11,41 +11,41 @@
           <h3 class="section-subheading text-muted">Veja como você pode ajudar</h3>
         </div>
       </div>
-      </div>
-    
+    </div>
+
   </section>
 
-    <h1 class="display-9  " style="color:red">POR QUE SER UM DOADOR?</h1>
-    <p class="lead">Beneficios de ser um doador</p>
-    <hr class="mb-4">
-    <div class="row">
-      <div class="col-4">
-        <div class="list-group list-group-horizontal " id="list-tab" role="tablist">
-          <a class="list-group-item list-group-item-action active" id="list-1-list" data-toggle="list" href="#list-1" role="tab" aria-controls="1">Uma doação de sangue pode salvar até 4 vidas</a>
-          <a class="list-group-item list-group-item-action" id="list-2-list" data-toggle="list" href="#list-2" role="tab" aria-controls="wild">Seu organismo repõe rapidamente o sangue doado</a>
-          <a class="list-group-item list-group-item-action" id="list-3-list" data-toggle="list" href="#list-3" role="tab" aria-controls="messages">Exame de Sangue Gratuito</a>
-          <a class="list-group-item list-group-item-action" id="list-4-list" data-toggle="list" href="#list-4" role="tab" aria-controls="messages">Ajudar outras pessoas faz bem para você também</a>
-        </div>
+  <h1 class="display-9  " style="color:red">POR QUE SER UM DOADOR?</h1>
+  <p class="lead">Beneficios de ser um doador</p>
+  <hr class="mb-4">
+  <div class="row">
+    <div class="col-4">
+      <div class="list-group list-group-horizontal " id="list-tab" role="tablist">
+        <a class="list-group-item list-group-item-action active" id="list-1-list" data-toggle="list" href="#list-1" role="tab" aria-controls="1">Uma doação de sangue pode salvar até 4 vidas</a>
+        <a class="list-group-item list-group-item-action" id="list-2-list" data-toggle="list" href="#list-2" role="tab" aria-controls="wild">Seu organismo repõe rapidamente o sangue doado</a>
+        <a class="list-group-item list-group-item-action" id="list-3-list" data-toggle="list" href="#list-3" role="tab" aria-controls="messages">Exame de Sangue Gratuito</a>
+        <a class="list-group-item list-group-item-action" id="list-4-list" data-toggle="list" href="#list-4" role="tab" aria-controls="messages">Ajudar outras pessoas faz bem para você também</a>
       </div>
-      <div class="col-6">
-        <div class="tab-content" id="nav-tabContent">
-          <div class="tab-pane fade show active" id="list-1" role="tabpanel" aria-labelledby="list-1-list">Após a coleta, o conteúdo da bolsa é fracionado em componentes sanguíneos (concentrado de hemácias, de plaquetas e plasma) que são examinados para que haja a certeza de que não existe a presença de nenhuma doença neles. Se há reação sorológica, a unidade é descartada. Caso contrário, os componentes são liberados e
-            podem ser usados em até quatro pessoas, ou seja, podem salvar até quatro vidas.</div>
-          <div class="tab-pane fade" id="list-2" role="tabpanel" aria-labelledby="list-2-list">Uma pessoa adulta, seja do sexo masculino ou feminino, possui em média cinco litros de sangue, e em uma doação são coletados no máximo 450 ml - ou seja, menos de 10% de todo o sangue.
-            Este volume é reposto em 24 horas pelo organismo.</div>
-          <div class="tab-pane fade" id="list-3" role="tabpanel" aria-labelledby="list-2-list">Quem deseja doar sangue passa por uma avaliação prévia para detectar se há condições ou doenças que possam ser impeditivas para a doação. Essa entrevista e esses exames são particulares e
-            os dados e resultados são mantidos sob total sigilo.</div>
-          <div class="tab-pane fade" id="list-4" role="tabpanel" aria-labelledby="list-2-list">Tanto quem recebe quanto quem doa sangue é beneficiado. A solidariedade é uma troca: psicologicamente,
-            a sensação de fazer o bem aos outros afeta positivamente o autor da ação.</div>
-        </div>
+    </div>
+    <div class="col-6">
+      <div class="tab-content" id="nav-tabContent">
+        <div class="tab-pane fade show active" id="list-1" role="tabpanel" aria-labelledby="list-1-list">Após a coleta, o conteúdo da bolsa é fracionado em componentes sanguíneos (concentrado de hemácias, de plaquetas e plasma) que são examinados para que haja a certeza de que não existe a presença de nenhuma doença neles. Se há reação sorológica, a unidade é descartada. Caso contrário, os componentes são liberados e
+          podem ser usados em até quatro pessoas, ou seja, podem salvar até quatro vidas.</div>
+        <div class="tab-pane fade" id="list-2" role="tabpanel" aria-labelledby="list-2-list">Uma pessoa adulta, seja do sexo masculino ou feminino, possui em média cinco litros de sangue, e em uma doação são coletados no máximo 450 ml - ou seja, menos de 10% de todo o sangue.
+          Este volume é reposto em 24 horas pelo organismo.</div>
+        <div class="tab-pane fade" id="list-3" role="tabpanel" aria-labelledby="list-2-list">Quem deseja doar sangue passa por uma avaliação prévia para detectar se há condições ou doenças que possam ser impeditivas para a doação. Essa entrevista e esses exames são particulares e
+          os dados e resultados são mantidos sob total sigilo.</div>
+        <div class="tab-pane fade" id="list-4" role="tabpanel" aria-labelledby="list-2-list">Tanto quem recebe quanto quem doa sangue é beneficiado. A solidariedade é uma troca: psicologicamente,
+          a sensação de fazer o bem aos outros afeta positivamente o autor da ação.</div>
       </div>
     </div>
   </div>
-  <p> posso ser um doador</p>
- <p> para aonde vai o sangue </p>
- <p> quais pessoas eu ajudo </p>
- <p>depoimento</p>
-  <!-- FIM JUMBOTRON "IMPORTANCIA DE DOAR SANGUE" -->
+</div>
+<p> posso ser um doador</p>
+<p> para aonde vai o sangue </p>
+<p> quais pessoas eu ajudo </p>
+<p>depoimento</p>
+<!-- FIM JUMBOTRON "IMPORTANCIA DE DOAR SANGUE" -->
 
 
-<?php include "footer.php"?>
+<?php include "footer.php" ?>
