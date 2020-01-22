c72e96fbf561aee8d6658685b9126844296a9338
kaiocampos <kaiocamposti@gmail.com>
2020-01-22 14:21:13 -0300

    Footer mais simplificado

--- www/view/header.php
+++ www/view/header.php
@@ -148,38 +148,4 @@
 
 
   <hr style="background-color: red; margin: 0px;">
-  <!-- MENU DE PRODUTOS -->
-  <div class="row justify-content-center ">
-    <!-- <div class="col-md-12 d-flex justify-content-center "> -->
-    <nav class="navbar navbar-expand-lg navbar-light p-0 d-none d-sm-block">
-
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <!-- IMPLEMENTAR LINKS PHP NO NAVLINK -->
-      <div class="collapse navbar-collapse " id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#"><i class="fas fa-tshirt"></i> Roupa<span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo BASEURL; ?>projeto"><i class="fas fa-gamepad"></i> Brinquedos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo BASEURL; ?>mapas"><i class="fas fa-sticky-note"></i> Adesivos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo BASEURL; ?>mapas"><i class="fas fa-laptop-code"></i>
-              Informatica</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo BASEURL; ?>mapas"><i class="fas fa-sticky-note"></i> Acessorios</a>
-          </li>
-
-        </ul>
-      </div>
-    </nav>
-  </div>
-  </div>
-  <!-- FIM MENU PRODUTOS -->
-  <hr style="background-color: red; margin: 0;" class="hrproduto d-none d-sm-block">
+  
---
+++ www/view/quem-somos.php
@@ -0,0 +1,125 @@
+    <!-- <footer class="footer text-center mt-5">
+      <div class="row">
+        <div class="w-100">
+          <h4 class="text-uppercase pt-3 pb-3">Construído por:</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md mb-4 mb-lg-0 ml-2">
+          <div class="lead mb-4 border rounded p-3 ">
+          <div class="mb-2">
+              <img src="https://avatars1.githubusercontent.com/u/33559500?s=400&u=ce99604febc48534d87e520b50024d4f42ab2889&v=4" alt="Roberto Rabelo" class="img-resposive w-25 img-circle border mr-3"> Roberto Rabelo
+              <br>
+              <small class="text-muted p-0 m-0">Front-end da pagina</small>
+            </div>
+            <ul class="list-inline">
+              <li class="list-inline-item ">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#" target="_blank">
+                  <i class="fab fa-telegram" style="color:#838B8B"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#" target="_blank">
+                  <i class="fab fa-whatsapp" style="color:#838B8B"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#" target="_blank">
+                  <i class="fab fa-linkedin" style="color:#838B8B"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#" target="_blank">
+                  <i class="fab fa-github" style="color:#838B8B"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md mb-4 mb-lg-0 ">
+          <div class="lead mb-4 border rounded p-3 ">
+            <div class="mb-2">
+              <img src="https://avatars0.githubusercontent.com/u/56602187?s=400&v=4" alt="William Marcedo" class="img-resposive w-25 img-circle border mr-3"> William Marcedo
+              <br>
+              <small class="text-muted">Back-end da pagina</small>
+            </div>
+            <ul class="list-inline">
+              <li class="list-inline-item ">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#" target="_blank">
+                  <i class="fab fa-telegram" style="color:#838B8B"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#" target="_blank">
+                  <i class="fab fa-whatsapp" style="color:#838B8B"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#" target="_blank">
+                  <i class="fab fa-linkedin" style="color:#838B8B"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#" target="_blank">
+                  <i class="fab fa-github" style="color:#838B8B"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md mb-4 mb-lg-0 mr-2">
+          <div class="lead mb-4 border rounded p-3 ">
+            <div class="mb-2">
+              <img src="https://avatars1.githubusercontent.com/u/29874680?s=400&v=4" alt="Kaio Campos" class="img-resposive w-25 img-circle border mr-3"> Kaio Campos
+              <br>
+              <small class="text-muted">Back-end da pagina</small>
+            </div>
+            <ul class="list-inline">
+              <li class="list-inline-item ">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#" target="_blank">
+                  <i class="fab fa-telegram" style="color:#838B8B"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#" target="_blank">
+                  <i class="fab fa-whatsapp" style="color:#838B8B"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#" target="_blank">
+                  <i class="fab fa-linkedin" style="color:#838B8B"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#" target="_blank">
+                  <i class="fab fa-github" style="color:#838B8B"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      </div>
+      <!-- Footer -->
+
+
+
+      <!-- FIM FOOTER -->
+
+
+      
+      <!-- Copyright -->
+
+
+      <div class="footer-copyright text-center py-3">© 2019 Copyright:
+        <a href="#"> Kaio, Roberto , William</a>
+      </div>
+      <!-- Copyright -->
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="<?php echo DIRJS; ?>jquery.js"></script>
+      <script src="<?php echo DIRJS; ?>bootstrap.bundle.js"></script>
+      <script src="<?php echo DIRJS; ?>bootstrap.js"></script>
+      </body>
+
+      </html> -->
