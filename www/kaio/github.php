5d291f38ad9b82ddc7740e5af4195bbe8b26ac8a
Kaio Campos <kaiocamposti@gmail.com>
2020-01-23 13:25:53 -0300

    retirado a pasta kaio

--- www/kaio/github.php
+++
@@ -1,3 +0,0 @@
-https://kaiocampos:Cerbero010203@github.com/kaiocampos/lojaphpunit.git
-remover o origin antigo e add o novo, quando tiver dando erro de permissao
-git remote add origin https://kaiocampos:Cerbero010203@github.com/kaiocampos/lojaphpunit.git
